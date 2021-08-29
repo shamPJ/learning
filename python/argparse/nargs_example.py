ef9736a80e2282f3a8c22251d1760c87cfda0ac2
Shamsi Abdurakhmanova <sham.abdurakh@gmail.com>
2021-08-29 23:16:28 +0300

    argparse; Hopfield network

--- python/argparse/nargs_example.py
+++ python/argparse/nargs_example.py
@@ -1,12 +1,86 @@
 # nargs_example.py
+# import argparse
+
+# my_parser = argparse.ArgumentParser()
+# my_parser.add_argument('input',
+#                        action='store',
+#                        nargs='?',
+#                        default='my default value')
+
+# args = my_parser.parse_args()
+
+# print(args.input)
+
+# However, the nargs keyword can also accept the following:
+
+# ?: a single value, which can be optional
+# *: a flexible number of values, which will be gathered into a list
+# +: like *, but requiring at least one value
+# argparse.REMAINDER: all the values that are remaining in the command line
+
+# So, for example, in the following program, the positional argument input takes a single value 
+# when provided, but if the value is not provided, then the one specified by the default keyword is used:
+
+# nargs_example.py
+# import argparse
+
+# my_parser = argparse.ArgumentParser()
+# my_parser.add_argument('input',
+#                        action='store',
+#                        nargs='?',
+#                        default='my default value')
+
+# args = my_parser.parse_args()
+
+# print(args.input)
+
+# $ python nargs_example.py 'my custom value'
+# my custom value
+
+# $ python nargs_example.py
+# my default value
+
+# nargs_example.py
+# import argparse
+
+# my_parser = argparse.ArgumentParser()
+# my_parser.add_argument('input',
+#                        action='store',
+#                        nargs='*',
+#                        default='my default value')
+
+# args = my_parser.parse_args()
+
+# print(args.input)
+
+# $ python nargs_example.py me you us
+# ['me', 'you', 'us']
+
+# $ python nargs_example.py
+# my default value
+
+# nargs_example.py
+# import argparse
+
+# my_parser = argparse.ArgumentParser()
+# my_parser.add_argument('input', action='store', nargs='+')
+
+# args = my_parser.parse_args()
+
+# print(args.input)
+
+# nargs_example.py
 import argparse
 
 my_parser = argparse.ArgumentParser()
-my_parser.add_argument('input',
-                       action='store',
-                       nargs='?',
-                       default='my default value')
+my_parser.add_argument('first', action='store')
+my_parser.add_argument('others', action='store', nargs=argparse.REMAINDER)
 
 args = my_parser.parse_args()
 
-print(args.input)
+print('first = %r' % args.first)
+print('others = %r' % args.others)
+
+# $ python nargs_example.py me you us
+# first = 'me'
+# others = ['you', 'us']
